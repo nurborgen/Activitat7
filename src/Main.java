49c4ee83300df8fa5fffa4052b6e018c6bca370a
Nuria Boronat <nubogen@gmail.com>
2022-12-15 20:43:23 +0100

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for(int i=0;i<=20;i++){
+            System.out.printf("%d ", i);
+        }
+
     }
 }
