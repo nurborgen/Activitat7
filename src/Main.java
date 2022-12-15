644b5adf15cebf3fbedd6824dc8942d16342e33b
Nuria Boronat <nubogen@gmail.com>
2022-12-15 20:46:13 +0100

    Saludem i anyadim descripció per al contador

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hola, el contador es:");
         for(int i=0;i<=20;i++){
             System.out.printf("%d ", i);
         }
